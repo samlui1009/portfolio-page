0b660fd503b01fca5c3513f1c41f20a3301280da
samlui1009 <sam_lui@hotmail.ca>
2025-06-27 14:33:17 -0700

    Removed external @emailjs/browser from Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            external: ['@emailjs/browser'],
-        }
-    },
   plugins: [react()],
 })

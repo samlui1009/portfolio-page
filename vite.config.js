c558d4ee03348cb0c272c7eedf21a3fc35f02e2e
samlui1009 <sam_lui@hotmail.ca>
2025-06-27 14:08:11 -0700

    Updates made to package.json for proper deployment via Netlify

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+    build: {
+        rollupOptions: {
+            external: ['@emailjs/browser'],
+        }
+    },
   plugins: [react()],
 })

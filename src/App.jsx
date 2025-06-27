bd541d443c57ec9962ec7e7ba318c22eae2fb3e9
samlui1009 <sam_lui@hotmail.ca>
2025-06-27 14:27:41 -0700

    Deleted redirects file, changed from BrowserRouter to HashRouter to troubleshoot Netlify deployment issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 // Handles routing
 
 import Home from './pages/Home.jsx';
